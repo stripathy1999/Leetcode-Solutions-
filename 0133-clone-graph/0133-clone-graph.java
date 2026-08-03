930cb3e8f6b7b63622e031a946e13ff7a01dff3a
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-08-03 00:47:04 -0700

    Time: 27 ms (25.73%), Space: 44 MB (94.86%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,25 +21,24 @@ class Node {
 class Solution {
     HashMap<Node, Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-        if(node == null){
+        if(node==null){
             return node;
         }
-
-        return dfs(node);
+        return dfs(node); // this means I am giving an original node from a graph, give me the cloned node of graph
     }
-
     public Node dfs(Node node){
         if(node == null){
             return node;
         }
 
         if(map.containsKey(node)){
-            return map.get(node);
+            return map.get(node); // this means the map found a clone of an original node, so it links up with the clone
         }
 
         Node clone = new Node(node.val);
         map.put(node, clone);
-
+        
+        //clone the neighbors of the given node and put them in map also
         for(Node neighbor : node.neighbors){
             Node clonedNeighbor = dfs(neighbor);
             clone.neighbors.add(clonedNeighbor);
