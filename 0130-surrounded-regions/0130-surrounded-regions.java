f9270186e6c2f7c0ed2e23d4baad9a09fc20a9a9
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-08-05 01:54:29 -0700

    Time: 2 ms (72.74%), Space: 47.9 MB (39.42%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,6 +1,8 @@
 class Solution {
     public void solve(char[][] board) {
         for(int col = 0; col<board[0].length; col++){
+            //Traverse from borders directly and turn all O's to T's because border O's cant be surrounded and captured. 
+            //Start from two ends of the column
             if(board[0][col] == 'O'){
                 dfs(0, col, board);
             }
@@ -8,7 +10,7 @@ class Solution {
                 dfs(board.length-1, col, board);
             }
         }
-
+        //Travser and start from two ends of the row and use dfs to those border O's to T's and any connected O cells to T as they cant be captured
         for(int row = 0; row<board.length; row++){
             if(board[row][0] == 'O'){
                 dfs(row, 0, board);
@@ -18,6 +20,7 @@ class Solution {
             }
         }
 
+        //After all the updates, switch all the T's back to O's and all the O's inside any other cells apart from borders to X's
         for(int row=0; row<board.length; row++){
             for(int col=0; col<board[0].length; col++){
                 if(board[row][col]=='O'){
