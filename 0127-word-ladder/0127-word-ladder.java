3dc1c08172c2c5d765e61d359cdf291dc03513cc
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-08-25 15:56:28 -0700

    Time: 92 ms (34.59%), Space: 49.4 MB (22.31%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,33 +1,31 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<String> queue = new LinkedList<>();
+        Queue<String> queue = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
-        Set<String> wordSet = new HashSet<>(wordList);
-        if(!wordSet.contains(endWord)) return 0;
+        Set<String> wordListSet= new HashSet<>(wordList);
 
+        if(!wordListSet.contains(endWord)) return 0;
+        
         queue.add(beginWord);
         visited.add(beginWord);
-
         int path = 1;
 
         while(!queue.isEmpty()){
-            int levelSize = queue.size();
-            for(int i=0; i<levelSize; i++){
+            int level = queue.size();
+            for(int i=0; i<level; i++){
                 String currentWord = queue.poll();
                 char[] wordChars = currentWord.toCharArray();
-
                 for(int j=0; j<wordChars.length; j++){
                     char originalChar = wordChars[j];
-
                     for(char c='a'; c<='z'; c++){
                         wordChars[j] = c;
                         String newWord = new String(wordChars);
-                        
-                        if(newWord.equals(endWord)) return path+1;
-
-                        if(wordSet.contains(newWord) && !visited.contains(newWord)){
-                            queue.add(newWord);
+                        if(newWord.equals(endWord)){
+                            return path+1;
+                        }
+                        if(wordListSet.contains(newWord) && !visited.contains(newWord)){
                             visited.add(newWord);
+                            queue.add(newWord);
                         }
                     }
                     wordChars[j] = originalChar;
