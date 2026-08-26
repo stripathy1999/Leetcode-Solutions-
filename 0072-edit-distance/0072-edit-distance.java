e92945aa0e17bfd86ceb97ac7c74dd38aaba9d1d
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-08-25 23:48:44 -0700

    Time: 6 ms (8.7%), Space: 47.1 MB (70.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,17 +1,22 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()+1][word2.length()+1];
-
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        //for base case, if word1 = "abc" and word2 = " " we have 3 deletions
         for(int i=0; i<=word1.length(); i++){
             dp[i][0] = i;
         }
+        //for base case, if word1 = " " and word2 = "abc" we have 3 insertions
         for(int i=0; i<=word2.length(); i++){
-            dp[0][i] = i; 
+            dp[0][i] = i;
         }
-        
+
+        //for base case handling replace, word1 = "abc" and word2 = "abc" 0 operations
+
+        //for getting the rest of the edit sequences
         for(int i=1; i<=word1.length(); i++){
             for(int j=1; j<=word2.length(); j++){
-
+                
+                //if both the strings are same, we just move our pointers
                 if(word1.charAt(i-1) == word2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
                 }
@@ -19,7 +24,7 @@ class Solution {
                     dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
                 }
             }
-        }
+        } 
         return dp[word1.length()][word2.length()];
-    } 
+    }
 }
