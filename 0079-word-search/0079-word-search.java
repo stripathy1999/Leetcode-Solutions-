851b2dddd503d82f33123c60709dabcda89373a4
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-09-04 02:12:32 -0700

    Time: 142 ms (44.48%), Space: 43 MB (60.08%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
         int char_pointer = 0;
+        boolean[][] visited = new boolean[board.length][board[0].length];
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
                 if(dfs(i, j, char_pointer, visited, board, word) == true){
@@ -12,26 +12,25 @@ class Solution {
         return false;
     }
     public boolean dfs(int row, int col, int pointer, boolean[][] visited, char[][] board, String word){
-        if(pointer == word.length()){
-            return true;
-        }
         if(row<0 || row >= board.length || col<0 || col >= board[0].length){
+            return false;
+        }
+        if(board[row][col] != word.charAt(pointer)){
             return false;
         }
         if(visited[row][col] == true){
             return false;
         }
-        if(word.charAt(pointer) != board[row][col]){
-            return false;
+        if(pointer == word.length()-1){
+            return true;
         }
-
+        
         visited[row][col] = true;
 
-        boolean found  = dfs(row+1, col, pointer+1, visited, board, word) || dfs(row-1, col, pointer+1, visited, board, word) || dfs(row, col+1, pointer+1, visited, board, word) || dfs(row, col-1, pointer+1, visited, board, word);
+        boolean found = dfs(row, col+1, pointer+1, visited, board, word) || dfs(row, col-1, pointer+1, visited, board, word) || dfs(row+1, col, pointer+1, visited, board, word) || dfs(row-1, col, pointer+1, visited, board, word);
 
         visited[row][col] = false;
-        
-        return found;
 
+        return found;
     }
 }
