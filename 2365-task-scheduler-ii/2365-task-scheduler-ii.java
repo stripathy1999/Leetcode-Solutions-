70db308589d3599b777f4d90993258d00f968d79
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-09-10 20:26:31 -0700

    Time: 41 ms (49.88%), Space: 97.4 MB (69.65%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long taskSchedulerII(int[] tasks, int space) {
+        HashMap<Integer, Long> map = new HashMap<>();
+        long day = 0;
+
+        for(int task: tasks){
+            if(map.containsKey(task)){
+                long nextAvailableDay = map.get(task);
+                
+                if(day < nextAvailableDay){
+                    day = nextAvailableDay;
+                }
+            }
+            map.put(task, space + day + 1);
+
+            day++;
+        }
+        return day;   
+    }
+}
