bc6348053a02cbd36b79cd921c839fc035ebca07
Sakshi Sanskruti Tripathy <stripathy1999@gmail.com>
2026-09-12 13:36:08 -0700

    Time: 7 ms (40.37%), Space: 47.2 MB (8.17%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,37 +1,39 @@
 class Solution {
     public double knightProbability(int n, int k, int row, int column) {
         double[][][] dp = new double[n][n][k+1];
-        
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 Arrays.fill(dp[i][j], -1.0);
             }
         }
-        
-        return dfs(row, column, n, k, dp);
+
+        return dfs(row, column, k, n, dp);
     }
-    public double dfs(int row, int col, int n, int k, double[][][] dp){
+    public double dfs(int row, int col, int moves, int n, double[][][] dp){
         if(row<0 || row>=n || col<0 || col>=n){
             return 0.0;
         }
-        if(k == 0){
+        if(moves == 0){
             return 1.0;
         }
-        if(dp[row][col][k] != -1.0){
-            return dp[row][col][k];
+        if(dp[row][col][moves] != -1.0){
+            return dp[row][col][moves];
         }
 
         int[][] directions = new int[][]{{1,2},{-1,2},{1,-2},{-1,-2},{2,1},{-2,1},{2,-1},{-2,-1}};
+
         double probability = 0.0;
+
         for(int[] direction : directions){
             int newRow = row + direction[0];
             int newCol = col + direction[1];
 
-            probability += dfs(newRow, newCol, n, k-1, dp)/8.0;
-
+            probability += dfs(newRow, newCol, moves-1, n, dp)/8.0;
         }
-        dp[row][col][k] = probability;
 
-        return probability;
+        dp[row][col][moves] = probability;
+
+        return dp[row][col][moves];
     }
 }
